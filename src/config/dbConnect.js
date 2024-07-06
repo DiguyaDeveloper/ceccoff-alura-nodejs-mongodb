109f00a674b801674b313d485d22f1d86bc06cc1
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-05 23:25:52 -0300

    feat: :sparkles: Create connection config with mongodb

---
+++ src/config/dbConnect.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import mongoose, { mongo } from "mongoose";
+
+async function databaseConnect() {
+    const user = process.env.DB_USER;
+    const password = process.env.DB_PASSWORD;
+    const cluster = process.env.DB_CLUSTER;
+    const dbName = process.env.DB_NAME;
+    const dbClusterName = process.env.DB_CLUSTER_NAME;
+
+    mongoose.connect(`mongodb+srv://${user}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority&appName=${dbClusterName}`);
+
+    return mongoose.connection;
+}
+
+export default databaseConnect;
