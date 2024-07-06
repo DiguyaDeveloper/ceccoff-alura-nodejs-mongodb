2d9a6a904ebf8e441b1fcebdd9a4c0df84e20a3a
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-05 22:29:30 -0300

    feat: :sparkles: Add routes empty, book and author

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ import http from "http";
 
 const PORT = 3000;
 
+const ROUTES = {
+    "/": "Curso de Express API",
+    "/book": "Entrei na rota livros",
+    "/author": "Entrei na rota autores"
+}
+
 const server = http.createServer((req, res) => {
 
     res.writeHead(200, { "Content-Type": "text/plain"});
-    res.end("Curso de Node.js");
+    res.end(ROUTES[req.url]);
 });
 
 server.listen(PORT, () => {
