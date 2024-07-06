113b05e7ae35fb63610ed3c36fa686f19a5f8df5
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-06 00:25:13 -0300

    feat: :sparkles: Update port to 8080

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import app from "./src/app.js";
+import app from './src/app.js';
 
-const PORT = 3000;
+const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server started in port: ${PORT}`)
+  console.log(`Server started in port: ${PORT}`);
 });
