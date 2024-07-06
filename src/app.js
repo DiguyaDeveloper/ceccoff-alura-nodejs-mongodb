68292988726c3d08d208220cc42ed672b9d590ac
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-05 22:48:16 -0300

    feat: :sparkles: Add route to get book by id

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 import express from "express";
 
 const books = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
+  {
+    id: 1,
+    titulo: "O Senhor dos Anéis",
+  },
+  {
+    id: 2,
+    titulo: "O Hobbit",
+  },
 ];
+
+function getBooks(id) {
+  return books.findIndex((book) => {
+    return book.id === Number(id);
+  });
+}
 
 const app = express();
 
@@ -17,17 +23,27 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-
-    res.status(200).send("Curso de Node.js");
+  res.status(200).send("Curso de Node.js");
 });
 
 app.get("/books", (req, res) => {
-    res.status(200).json(books);
+  res.status(200).json(books);
 });
 
 app.post("/books", (req, res) => {
-    books.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso");
+  books.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso");
 });
+
+app.get("/books/:id", (req, res) => {
+  const index = getBooks(req.params.id);
+
+  if (book) {
+    res.status(200).json(books[index]);
+  }
+
+  res.status(404).send("Livro não encontrado");
+});
+
 
 export default app;
