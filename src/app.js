8bb1e13490e0bc21d7bf53a8db2584552495f2dc
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-05 22:38:49 -0300

    feat: :sparkles: Add route to get array of books

--- src/app.js
+++ src/app.js
@@ -7,4 +7,18 @@ app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
 });
 
+app.get("/books", (req, res) => {
+    const books = [
+        {
+            id: 1,
+            titulo: "O Senhor dos Anéis"
+        },
+        {
+            id: 2,
+            titulo: "O Hobbit"
+        }
+    ]
+    res.status(200).json(books);
+})
+
 export default app;
